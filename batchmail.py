1f991b6f8c8938ee7ab2875b9d6058227a9c2ebd
hupantingxue <hupantingxue@126.com>
2014-02-24 16:31:56 +0800

    Batch send mail one by one;

--- batchmail.py
+++ batchmail.py
@@ -3,6 +3,7 @@
 
 import smtplib
 from email.mime.text import MIMEText
+import time
 import conf
 
 mail_body = conf.MAIL_BODY
@@ -11,15 +12,19 @@ mail_to = conf.MAIL_TO
 mail_title = conf.MAIL_TITLE
 
 def send_mail(_from, to_list, title, content):
-    msg = MIMEText(content)
-    msg['Subject'] = title
-    msg['From'] = _from
-    msg['To'] = ';'.join(to_list)
-
     smtp = smtplib.SMTP()
     smtp.connect(conf.SMTP_SVR)
     smtp.login(conf.USER, conf.PWD)
-    smtp.sendmail(mail_from, mail_to, msg.as_string())
+
+    for to_item in to_list:
+        msg = MIMEText(content)
+        msg['Subject'] = title
+        msg['From'] = _from
+        msg['To'] = ''
+        msg['To'] = to_item
+        smtp.sendmail(mail_from, to_item, msg.as_string())
+        print '%s send ok' % (to_item)
+        time.sleep(0.01)
     smtp.quit()
 
 if __name__ == '__main__':
